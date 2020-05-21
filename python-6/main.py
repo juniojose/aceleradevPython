ad26112368e9a82e0bedca2e72570d54fd478223
Junio Jose <juniojose@gmail.com>
2020-05-21 20:57:34 -0300

    Desafio python-6 finalizado

--- python-6/main.py
+++ python-6/main.py
@@ -1,39 +1,67 @@
+from abc import ABC, abstractmethod
+
+
 class Department:
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
+    def get_hours(self):
+        return 8
 
-class Employee:
+
+class Employee(ABC):
     def __init__(self, code, name, salary):
         self.code = code
         self.name = name
         self.salary = salary
 
+    @abstractmethod
     def calc_bonus(self):
         pass
 
+    @abstractmethod
     def get_hours(self):
-        pass
+        return 8
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
+        self.__department = Department('managers', 1)
 
     def calc_bonus(self):
         return self.salary * 0.15
 
+    def set_department(self, department):
+        self.__department.name = department
 
-class Seller(Manager):
-    def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.sales = 0
+    def get_departament(self):
+        return self.__department.name
 
     def get_hours(self):
-        return 6
+        return 8
 
-teste = Employee('123', 'JJ', 6000)
-print(teste)
+class Seller(Employee):
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary)
+        self.__department = Department('sellers', 2)
+        self.__sales = 0
+
+    def put_sales(self, value):
+        self.__sales += value
+
+    def get_sales(self):
+        return self.__sales
+
+    def calc_bonus(self):
+        return self.__sales * 0.15
+
+    def get_hours(self):
+        return 8
+
+    def set_department(self, department):
+        self.__department.name = department
+
+    def get_departament(self):
+        return self.__department.name
